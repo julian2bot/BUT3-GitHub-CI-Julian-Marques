3f58e979d9f93b42b571ddb33d374d987cbe7527
julian <marquesjulian26@gmail.com>
2025-09-29 19:02:05 +0200

    si je run comme ca ?

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,3 +4,4 @@ mkdir -p /usr/share/nginx/html
 # Remplacer __ENV__ par la variable d’environnement passée par docker-compose
 sed "s/__ENV__/${ENV}/g" /constants.template.tsx > /usr/share/nginx/html/constants.tsx
 
+nginx -g 'daemon off;'
--- frontend/web.dockerfile
+++ frontend/web.dockerfile
@@ -21,4 +21,4 @@ COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/bin/sh", "/entrypoint.sh"]
-CMD ["nginx", "-g", "daemon off;"]
+# CMD ["nginx", "-g", "daemon off;"]
