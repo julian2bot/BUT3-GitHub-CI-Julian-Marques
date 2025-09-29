d84fc9ab47ae908bdac7f4632c3556fc2fb237bd
julian <marquesjulian26@gmail.com>
2025-09-29 18:58:30 +0200

    creer le fichier si non existant mais ca va rien changer x)

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p /usr/share/nginx/html
 # Remplacer __ENV__ par la variable d’environnement passée par docker-compose
 sed "s/__ENV__/${ENV}/g" /constants.template.tsx > /usr/share/nginx/html/constants.tsx
 
