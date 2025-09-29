84486fb34ccc5263d4d42c7b804f9b9da9da42b6
julian <marquesjulian26@gmail.com>
2025-09-29 19:28:29 +0200

    hmmm comment je rename mes __env__ ducoup?

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p /usr/share/nginx/html
+# mkdir -p /usr/share/nginx/html
 # Remplacer __ENV__ par la variable d’environnement passée par docker-compose
-sed "s/__ENV__/test/g" /constants.template.tsx > /usr/share/nginx/html/constants.tsx
+# sed "s/__ENV__/test/g" /constants.template.tsx > /usr/share/nginx/html/constants.tsx
 
 nginx -g 'daemon off;'
